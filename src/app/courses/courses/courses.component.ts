b12f6d3c900d1f60be2cfe9039e753dca4d93240
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-07-11 14:59:04 -0300

    Inicia projeto crud-angular-spring

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,31 @@
+import { CoursesService } from './../services/courses.service';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  // courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    // this.courses = [];
+    // this.coursesService = new CoursesService();
+     this.courses$ = this.coursesService.list();
+
+    // this.coursesService.list().subscribe(courses => this.courses =);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
